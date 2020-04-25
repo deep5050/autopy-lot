6283a40d6556978a15c675d8c53219808102f55f
Dipankar Pal <dipankarpal5050@gmail.com>
2020-04-25 20:58:21 +0000

    entrypoint updated

--- src/entrypoint.py
+++ src/entrypoint.py
@@ -9,8 +9,9 @@ GITHUB_EVENT_NAME = os.environ['GITHUB_EVENT_NAME']
 
 # Set repository
 CURRENT_REPOSITORY = os.environ['GITHUB_REPOSITORY']
-TARGET_REPOSITORY = os.environ['INPUT_TARGET_REPOSITORY'] or CURRENT_REPOSITORY  # TODO: How about PRs from forks?
-PULL_REQUEST_REPOSITORY = os.environ['INPUT_PULL_REQUEST_REPOSITORY'] or TARGET_REPOSITORY 
+# TODO: How about PRs from forks?
+TARGET_REPOSITORY = os.environ['INPUT_TARGET_REPOSITORY'] or CURRENT_REPOSITORY
+PULL_REQUEST_REPOSITORY = os.environ['INPUT_PULL_REQUEST_REPOSITORY'] or TARGET_REPOSITORY
 REPOSITORY = PULL_REQUEST_REPOSITORY if GITHUB_EVENT_NAME == 'pull_request' else TARGET_REPOSITORY
 
 # Set branches
@@ -28,19 +29,20 @@ GITHUB_TOKEN = os.environ['INPUT_GITHUB_TOKEN']
 
 # command related inputs
 CHECK = os.environ['INPUT_CHECK']  # 'all' | 'latest'
-INPUT_TYPE = os.environ['INPUT_TYPE'] # [ py , ipynb, r]
-COMMENT_MAGICS = os.environ['INPUT_COMMENT_MAGICS'] # [ true ]
-SPLIT_AT_HEADING = os.environ['INPUT_SPLIT_AT_HEADING'] # [ true ]
-OUTPUT_TYPE = os.environ['INPUT_OUTPUT_TYPE'] # [ py, ipynb, markdown, rmarkdown, r ]
-
-
-
+INPUT_TYPE = os.environ['INPUT_TYPE']  # [ py , ipynb, r]
+COMMENT_MAGICS = os.environ['INPUT_COMMENT_MAGICS']  # [ true ]
+SPLIT_AT_HEADING = os.environ['INPUT_SPLIT_AT_HEADING']  # [ true ]
+# [ py, ipynb, markdown, rmarkdown, r ]
+OUTPUT_TYPE = os.environ['INPUT_OUTPUT_TYPE']
 
 
 def main():
-  
+
     if (GITHUB_EVENT_NAME == 'pull_request') and (GITHUB_ACTOR != GITHUB_REPOSITORY_OWNER):
         return
+
+    command = ''
+    command = prepare_command()
 
     if CHECK:
         if CHECK == 'all':
@@ -48,7 +50,8 @@ def main():
         elif CHECK == 'latest':
             files = get_modified_files()
         else:
-            raise ValueError(f'{CHECK} is a wrong value. Expecting all or latest')
+            raise ValueError(
+                f'{CHECK} is a wrong value. Expecting all or latest')
     else:
         files = []
 
@@ -64,4 +67,32 @@ def main():
     else:
         print('There is no modified notebooks in a current commit.')
 
+
+
+
+
+
+
+def prepare_command() -> str:
+    pass
+
+
+def get_all_files() -> iter:
+    pass
+
+
+def get_modified_files() -> list:
+    pass
+
+
+def check_file(files: list) -> list:
+    pass
+
+
+def commit_changes(nbs: list):
+    pass
+
+
+def push_changes():
+    pass
 
