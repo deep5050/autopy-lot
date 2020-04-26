9d264fcc417fa37a440f1516b18c83bd7f3f50bc
Dipankar Pal <dipankarpal5050@gmail.com>
2020-04-26 14:04:34 +0530

    Update entrypoint.py

--- src/entrypoint.py
+++ src/entrypoint.py
@@ -29,11 +29,114 @@ GITHUB_TOKEN = os.environ['INPUT_GITHUB_TOKEN']
 
 # command related inputs
 CHECK = os.environ['INPUT_CHECK']  # 'all' | 'latest'
-INPUT_TYPE = os.environ['INPUT_TYPE']  # [ py , ipynb, r]
+INPUT_TYPE = os.environ['INPUT_INPUT_TYPE']  # [ py , ipynb, r]
 COMMENT_MAGICS = os.environ['INPUT_COMMENT_MAGICS']  # [ true ]
-SPLIT_AT_HEADING = os.environ['INPUT_SPLIT_AT_HEADING']  # [ true ]
-# [ py, ipynb, markdown, rmarkdown, r ]
+# [ true ] [ py, ipynb, markdown, rmarkdown, r ]
+SPLIT_AT_HEADING = os.environ['INPUT_SPLIT_AT_HEADING']
 OUTPUT_TYPE = os.environ['INPUT_OUTPUT_TYPE']
+OUTPUT_DIR = os.environ['INPUT_OUTPUT_DIR']
+
+OUTPUT_EXT = 'py'
+
+
+def prepare_command() -> str:
+
+    command = "jupytext"
+
+    if COMMENT_MAGICS:
+        if COMMENT_MAGICS == 'true':
+            command = command + " --opt comment_magics=true "
+            print(command)
+        else:
+            print("supported: --opt command_magics: true")
+
+    if SPLIT_AT_HEADING:
+        if SPLIT_AT_HEADING == 'true':
+            command = command + " --opt split_at_heading=true "
+        else:
+            print("supported: --opt split_at_heading=true")
+
+    if OUTPUT_TYPE:
+        if OUTPUT_TYPE == 'py':
+            command = command + ' --to py'
+            OUTPUT_EXT = 'py'
+
+        if OUTPUT_TYPE == 'ipynb':
+            command = command + ' --to ipynb'
+            OUTPUT_EXT = 'ipynb'
+
+        if OUTPUT_TYPE == 'markdown':
+            command = command + ' --to markdown'
+            OUTPUT_EXT = 'md'
+
+        if OUTPUT_TYPE == 'r':
+            command = command + ' --to r'
+            OUTPUT_EXT = 'R'
+
+    return command
+
+
+def get_all_files() -> iter:
+    """Get list of all the input files in  this repo.
+    """
+    files = iglob(f'**/*.{INPUT_TYPE}', recursive=True)
+    return files
+
+
+def get_modified_files() -> list:
+    """Get list of all the modified files only in a current commit.
+    """
+    cmd = 'git diff-tree --no-commit-id --name-only -r HEAD'
+    committed_files = sp.getoutput(cmd).split('\n')
+    files = [file for file in committed_files if (
+        file.endswith(f'.{INPUT_TYPE}') and os.path.isfile(file))]
+    return files
+
+
+def convert_files(files: list, command: str) -> list:
+    """Iterates over input file list.
+    """
+    output_files = []
+    for file in files:
+        output_file_name = f'{OUTPUT_DIR}/{os.path.splitext(file)[0]}.{OUTPUT_EXT}'
+        output_files.append(output_file_name)
+
+        temp_command = command + f' {file} -o {output_file_name}'
+
+        # run this command
+        sp.call(temp_command, shell=True)
+        print(f"converting :{file} -> {output_file_name} ")
+
+    return output_files
+
+
+def commit_changes(files: list):
+    """Commits changes.
+    """
+    set_email = 'git config --local user.email "autopy-lot@master"'
+    set_user = 'git config --local user.name "autopy-lot action"'
+
+    sp.call(set_email, shell=True)
+    sp.call(set_user, shell=True)
+    file_list = ' '.join(set(files))
+
+    git_checkout = f'git checkout {TARGET_BRANCH}'
+    git_add = f'git add {file_list}'
+    git_commit = 'git commit -m "Add/Update autopy-lot outputs"'
+    print(f'Committing {file_list}...')
+
+    sp.call(git_checkout, shell=True)
+    sp.call(git_add, shell=True)
+    sp.call(git_commit, shell=True)
+
+
+def push_changes():
+    """Pushes commit.
+    """
+    set_url = f'git remote set-url origin https://x-access-token:{GITHUB_TOKEN}@github.com/{TARGET_REPOSITORY}'
+    git_push = f'git push origin {TARGET_BRANCH}'
+    sp.call(set_url, shell=True)
+    sp.call(git_push, shell=True)
 
 
 def main():
@@ -41,14 +144,13 @@ def main():
     if (GITHUB_EVENT_NAME == 'pull_request') and (GITHUB_ACTOR != GITHUB_REPOSITORY_OWNER):
         return
 
-    command = ''
     command = prepare_command()
 
     if CHECK:
         if CHECK == 'all':
-            files = get_all_files()
+            files = get_all_files()  # select all specifies input files
         elif CHECK == 'latest':
-            files = get_modified_files()
+            files = get_modified_files()  # select files only modified in the last commit
         else:
             raise ValueError(
                 f'{CHECK} is a wrong value. Expecting all or latest')
@@ -56,43 +158,17 @@ def main():
         files = []
 
     if files:
-        modified_files = check_file(files)
-        if modified_files:
+        output_files = convert_files(files, command)
+        if output_files:
             # Commit changes
-            commit_changes(modified_files)
+            commit_changes(output_files)  # add and commit the changes
             # Push
-            push_changes()
+            push_changes()  # push the changes back
         else:
             print('Nothing to add. Nothing to update!')
     else:
-        print('There is no modified notebooks in a current commit.')
+        print('There is no modified input files in the current commit.')
 
 
-
-
-
-
-
-def prepare_command() -> str:
-    pass
-
-
-def get_all_files() -> iter:
-    pass
-
-
-def get_modified_files() -> list:
-    pass
-
-
-def check_file(files: list) -> list:
-    pass
-
-
-def commit_changes(nbs: list):
-    pass
-
-
-def push_changes():
-    pass
-
+if __name__ == '__main__':
+    main()
