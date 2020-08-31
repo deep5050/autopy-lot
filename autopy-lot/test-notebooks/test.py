4d2f5227e63fdfe9262c494c423a1ddca683f26a
Restyled.io <commits@restyled.io>
2020-08-31 16:47:43 +0000

    Restyled by whitespace

--- autopy-lot/test-notebooks/test.py
+++ autopy-lot/test-notebooks/test.py
@@ -51,16 +51,16 @@ def preprocess_strong_suffix(shift, bpos, pat, m):
 
     while i > 0:
 
-        """if character at position i-1 is 
-        not equivalent to character at j-1, 
-        then continue searching to right 
+        """if character at position i-1 is
+        not equivalent to character at j-1,
+        then continue searching to right
         of the pattern for border """
         while j <= m and pat[i - 1] != pat[j - 1]:
 
-            """ the character preceding the occurrence 
-            of t in pattern P is different than the 
-            mismatching character in P, we stop skipping 
-            the occurrences and shift the pattern 
+            """ the character preceding the occurrence
+            of t in pattern P is different than the
+            mismatching character in P, we stop skipping
+            the occurrences and shift the pattern
             from i to j """
             if shift[j] == 0:
                 shift[j] = j - i
@@ -68,7 +68,7 @@ def preprocess_strong_suffix(shift, bpos, pat, m):
             # Update the position of next border
             j = bpos[j]
 
-        """ p[i-1] matched with p[j-1], border is found. 
+        """ p[i-1] matched with p[j-1], border is found.
 		store the beginning position of border """
         i -= 1
         j -= 1
@@ -82,20 +82,20 @@ def preprocess_case2(shift, bpos, pat, m):
     j = bpos[0]
     for i in range(m + 1):
 
-        """ set the border position of the first character 
-        of the pattern to all indices in array shift 
+        """ set the border position of the first character
+        of the pattern to all indices in array shift
         having shift[i] = 0 """
         if shift[i] == 0:
             shift[i] = j
 
-        """ suffix becomes shorter than bpos[0], 
-		use the position of next widest border 
+        """ suffix becomes shorter than bpos[0],
+		use the position of next widest border
 		as value of j """
         if i == j:
             j = bpos[j]
 
 
-"""Search for a pattern in given text using 
+"""Search for a pattern in given text using
 Boyer Moore algorithm with Good suffix rule """
 
 
@@ -117,12 +117,12 @@ def BMPsearch_good(pat, text):
     while s <= n - m:
         j = m - 1
 
-        """ Keep reducing index j of pattern while characters of 
+        """ Keep reducing index j of pattern while characters of
       pattern and text are matching at this shift s"""
         while j >= 0 and pat[j] == text[s + j]:
             j -= 1
 
-        """ If the pattern is present at the current shift, 
+        """ If the pattern is present at the current shift,
       then index j will become -1 after the above loop """
         if j < 0:
             # print("pattern occurs at shift = %d" % s)
@@ -130,7 +130,7 @@ def BMPsearch_good(pat, text):
             s += shift[0]
         else:
 
-            """pat[i] != pat[s+j] so shift the pattern 
+            """pat[i] != pat[s+j] so shift the pattern
             shift[j+1] times """
             s += shift[j + 1]
     return pos
@@ -146,7 +146,7 @@ def BMPsearch_good(pat, text):
 
 def find_repeats(dna, n):
     """
-    This help function for repeats_identifier find and count repeats for 
+    This help function for repeats_identifier find and count repeats for
     each dna sequence
     dna: sequence, string
     n: number of repeats, int
@@ -338,7 +338,7 @@ def common_loc_pattern(patterns, path):
 ##################
 def find_repeats(data, n):
     """
-    This help function for repeats_identifier find and count repeats for 
+    This help function for repeats_identifier find and count repeats for
     each dna sequence
     dna: sequence, string
     n: number of repeats, int
